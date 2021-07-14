37e4bba88332efdac2720847e0bbf140ce338aaa
savsrin <savsrin@gmail.com>
2021-07-14 10:18:11 -0700

    added create fragment

--- app/src/main/java/com/example/personalproject/activities/MainActivity.java
+++ app/src/main/java/com/example/personalproject/activities/MainActivity.java
@@ -1,22 +1,60 @@
 package com.example.personalproject.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.example.personalproject.R;
 import com.example.personalproject.databinding.ActivityMainBinding;
+import com.example.personalproject.fragments.CreateFragment;
+import com.example.personalproject.fragments.DashboardFragment;
+import com.example.personalproject.fragments.HomeFragment;
+import com.example.personalproject.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_create:
+                        fragment = new CreateFragment();
+                        break;
+                    case R.id.action_dashboard:
+                        fragment = new DashboardFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        // Set default selection
+        binding.bottomNavigationView.setSelectedItemId(R.id.action_home);
+
 
 
         binding.btnLogout.setOnClickListener(new View.OnClickListener() {
