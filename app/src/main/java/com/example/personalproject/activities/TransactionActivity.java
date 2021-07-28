97fca9cba1512c14270a18f8a50207eccbe4b121
savsrin <savsrin@gmail.com>
2021-07-28 13:40:29 -0700

    Added Parse Live Query client to Transaction Activity and subscribed to notifications for updates to the fields of the Item the buyer is currently considering purchasing. Upon an update event, I added logic to see whether the item is still available; if it’s no longer available, the button is disabled & the user gets a toast notifying them that the item has already gone pending. I’ve tested this on several user flow cases involving different parts of the Android lifecycle.

--- app/src/main/java/com/example/personalproject/activities/TransactionActivity.java
+++ app/src/main/java/com/example/personalproject/activities/TransactionActivity.java
@@ -5,7 +5,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -17,22 +20,37 @@ import com.example.personalproject.databinding.ActivityMainBinding;
 import com.example.personalproject.databinding.ActivityTransactionBinding;
 import com.example.personalproject.models.Item;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.livequery.ParseLiveQueryClient;
+import com.parse.livequery.SubscriptionHandling;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class TransactionActivity extends AppCompatActivity {
-    private static final String TAG = "TransactionActivity";
+    public static final String TAG = "TransactionActivity";
+
+    ParseLiveQueryClient parseLiveQueryClient = null;
+    SubscriptionHandling<Item> subscriptionHandling = null;
+    ParseQuery<Item> subscribedQuery = null;
+
     ActivityTransactionBinding binding;
     ParseUser buyer;
     ParseUser seller;
     Item item;
     String buyerPhone;
     String buyerEmail;
+    Boolean isStopped;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "in onCreate");
         super.onCreate(savedInstanceState);
         binding = ActivityTransactionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
         if (getIntent().getExtras() != null) {
             buyer = getIntent().getParcelableExtra("buyer");
             item = getIntent().getParcelableExtra("item");
@@ -42,6 +60,48 @@ public class TransactionActivity extends AppCompatActivity {
         }
 
         initView();
+        
+        try {
+            parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient(new URI("wss://fashionmarketplace.b4a.io/"));
+            Log.i(TAG, "parse query client created");
+        } catch (URISyntaxException e) {
+            Log.i(TAG, "Exception fom initializing parse Live Query Client.");
+            e.printStackTrace();
+        }
+
+        if (parseLiveQueryClient != null) {
+            ParseQuery<Item> parseQuery = ParseQuery.getQuery(Item.class);
+            parseQuery.whereEqualTo("objectId", item.getObjectId());
+            subscribedQuery = parseQuery;
+
+            subscriptionHandling = parseLiveQueryClient.subscribe(parseQuery);
+            Log.i(TAG, "Subscription of live query done.");
+            subscriptionHandling.handleEvent(SubscriptionHandling.Event.UPDATE, new SubscriptionHandling.HandleEventCallback<Item>() {
+                @Override
+                public void onEvent(ParseQuery<Item> query, Item updatedItem) {
+                    if (updatedItem.getStatus() == Item.AVAILABLE) {
+                        Log.i(TAG, "Updated item is still available");
+                        return;
+                    }
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Log.i(TAG, "in main looper, UPDATE event when item is not available");
+                            binding.btnTrans.setEnabled(false);
+                            binding.btnTrans.setBackgroundColor(Color.GRAY);
+                            if (!isStopped) {
+                                Toast.makeText(TransactionActivity.this,
+                                        "Sorry this item is no longer available.",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                            Log.i(TAG, "Unsubscribing live query");
+                            parseLiveQueryClient.unsubscribe(subscribedQuery);
+                            subscribedQuery = null;
+                        }
+                    });
+                }
+            });
+        }
 
         binding.btnTrans.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,9 +120,34 @@ public class TransactionActivity extends AppCompatActivity {
                 Log.i(TAG, buyerPhone);
                 Log.i(TAG, buyerEmail);
 
-                // TODO: Implement purchase method
+                // TODO: update database with transaction information & change item status to pending
+                // TODO: Then, navigate user to Purchases Tab of Dashboard
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "in onDestroy");
+        if (parseLiveQueryClient != null && subscribedQuery != null) {
+            Log.i(TAG, "Unsubscribing live query");
+            parseLiveQueryClient.unsubscribe(subscribedQuery);
+            subscribedQuery = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isStopped = false;
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "in onStop");
+        super.onStop();
+        isStopped = true;
     }
 
     private void initView() {
