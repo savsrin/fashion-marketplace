cbbfcfa0fdcb77a6d023ebcaaa04bd554d002019
savsrin <savsrin@gmail.com>
2021-07-30 15:58:24 -0700

    Added the Sales Tab which displays the current users’s items for sale in order of status and published date. I added the UI and backend logic to query and display the sales items from Parse with accounting for real-time item updates/additions using the Parse LiveQuery client. A remaining item which will follow in the next commit is adding the backend for the seller to take the confirmation/cancellation actions.

---
+++ app/src/main/java/com/example/personalproject/adapters/SalesAdapter.java
@@ -0,0 +1,176 @@
+package com.example.personalproject.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.personalproject.databinding.ItemSaleTransactionBinding;
+import com.example.personalproject.models.Item;
+import com.example.personalproject.models.Transaction;
+import com.parse.DeleteCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+public class SalesAdapter extends RecyclerView.Adapter<SalesAdapter.ViewHolder>{
+    public final String TAG = "SalesAdapter";
+    private List<Item> items;
+    private Context context;
+
+    public SalesAdapter(Context context, List<Item> items){
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemSaleTransactionBinding binding = ItemSaleTransactionBinding.inflate(
+                LayoutInflater.from(context),
+                parent,
+                false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SalesAdapter.ViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void handleItemAdded(Item addedItem) {
+        addItem(addedItem);
+        notifyDataSetChanged();
+    }
+
+    public void handleItemUpdate(Item updatedItem) {
+        if (removeItem(updatedItem)) {
+            addItem(updatedItem);
+            notifyDataSetChanged();
+        }
+    }
+
+    private boolean removeItem(Item itemToRemove) {
+        for (int i = 0 ; i < items.size(); i++) {
+            Item curItem = items.get(i);
+            if (curItem.getObjectId().equals(itemToRemove.getObjectId())) {
+                items.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void addItem(Item itemToAdd) {
+        for (int i = 0; i < items.size(); i++) {
+            Item curItem = items.get(i);
+            if (itemToAdd.getStatus() <= curItem.getStatus() &&
+                itemToAdd.getCreatedAt().compareTo(curItem.getCreatedAt()) >= 0) {
+                items.add(i, itemToAdd);
+                return;
+            }
+        }
+        items.add(itemToAdd);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ItemSaleTransactionBinding itemSaleTransactionBinding;
+
+        public ViewHolder(@NonNull ItemSaleTransactionBinding itemSaleTransactionBinding) {
+            super(itemSaleTransactionBinding.getRoot());
+            this.itemSaleTransactionBinding = itemSaleTransactionBinding;
+        }
+
+        public void bind(Item item) {
+            setBackgroundColor(item);
+            itemSaleTransactionBinding.tvSalesTransacTitle.setText(item.getDisplayName());
+            itemSaleTransactionBinding.tvPriceSalesTransac.setText("List Price: $"
+                                                                    + item.getPrice().toString());
+            ParseFile image = item.getPhoto();
+            if (image != null) {
+                Glide.with(context).load(image.getUrl()).into(itemSaleTransactionBinding.ivSalesTransacImage);
+            }
+            if (item.getStatus() == Item.PENDING) {
+                initPendingItemView(item);
+
+            } else {
+                initNonPendingItemView(item);
+            }
+
+            itemSaleTransactionBinding.btnConfirmPayment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /* TODO: change item status to sold & update transaction to paid;
+                             Note that the UI is updated when the live subscription gets an
+                             update notification.
+                     */
+                }
+            });
+
+            itemSaleTransactionBinding.btnCancelTransaction.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /* TODO: change item status to available, remove transaction from item, &
+                            delete transaction; Note that the UI is updated when the live subscription
+                            gets an update notification.
+                     */
+                }
+            });
+        }
+
+        private void initPendingItemView(Item item) {
+            itemSaleTransactionBinding.tvBuyerEmail.setText("Email"
+                    + item.getTransaction().getBuyerEmail());
+            itemSaleTransactionBinding.tvBuyerName.setText("Name: "
+                    + item.getTransaction().getBuyer().get("name"));
+            itemSaleTransactionBinding.tvBuyerPhoneSalesTransac.setText("Phone: "
+                    + item.getTransaction().getBuyerPhone());
+            itemSaleTransactionBinding.btnConfirmPayment.setVisibility(View.VISIBLE);
+            itemSaleTransactionBinding.btnConfirmPayment.setVisibility(View.VISIBLE);
+            itemSaleTransactionBinding.tvBuyerEmail.setVisibility(View.VISIBLE);
+            itemSaleTransactionBinding.tvBuyerName.setVisibility(View.VISIBLE);
+            itemSaleTransactionBinding.tvBuyerPhoneSalesTransac.setVisibility(View.VISIBLE);
+        }
+
+        private void initNonPendingItemView(Item item) {
+            itemSaleTransactionBinding.btnCancelTransaction.setVisibility(View.INVISIBLE);
+            itemSaleTransactionBinding.btnConfirmPayment.setVisibility(View.INVISIBLE);
+            itemSaleTransactionBinding.btnConfirmPayment.setVisibility(View.INVISIBLE);
+            itemSaleTransactionBinding.tvBuyerDetails.setVisibility(View.INVISIBLE);
+            itemSaleTransactionBinding.tvBuyerEmail.setVisibility(View.INVISIBLE);
+            itemSaleTransactionBinding.tvBuyerName.setVisibility(View.INVISIBLE);
+            itemSaleTransactionBinding.tvBuyerPhoneSalesTransac.setVisibility(View.INVISIBLE);
+        }
+
+        private void setBackgroundColor(Item item) {
+            if (item.getStatus() == Item.AVAILABLE) {
+                itemSaleTransactionBinding.cardViewSalesTransac
+                        .setCardBackgroundColor(Color.parseColor("#C7F09D"));
+                itemSaleTransactionBinding.tvStatusSalesTransac.setText("AVAILABLE");
+            } else if (item.getStatus() == Item.PENDING) {
+                itemSaleTransactionBinding.cardViewSalesTransac
+                        .setCardBackgroundColor(Color.parseColor("#F0F09D"));
+                itemSaleTransactionBinding.tvStatusSalesTransac.setText("PENDING");
+            } else {
+                itemSaleTransactionBinding.cardViewSalesTransac
+                        .setCardBackgroundColor(Color.parseColor("#F09D9D"));
+                itemSaleTransactionBinding.tvStatusSalesTransac.setText("SOLD");
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/personalproject/fragments/SalesFragment.java
@@ -0,0 +1,171 @@
+package com.example.personalproject.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.personalproject.R;
+import com.example.personalproject.adapters.SalesAdapter;
+import com.example.personalproject.databinding.FragmentDashboardBinding;
+import com.example.personalproject.databinding.FragmentHomeBinding;
+import com.example.personalproject.databinding.FragmentSalesBinding;
+import com.example.personalproject.models.Item;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.livequery.ParseLiveQueryClient;
+import com.parse.livequery.SubscriptionHandling;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SalesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SalesFragment extends Fragment {
+
+    public static final String TAG = "SalesFragment";
+
+    ParseLiveQueryClient parseLiveQueryClient = null;
+    SubscriptionHandling<Item> subscriptionHandling = null;
+    ParseQuery<Item> subscribedQuery = null;
+
+    private FragmentSalesBinding binding;
+    private List<Item> items = new ArrayList<>();;
+    private SalesAdapter adapter;
+
+    public SalesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "in on resume");
+        super.onResume();
+        if (parseLiveQueryClient == null) {
+            try {
+                parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient(
+                        new URI("wss://fashionmarketplace.b4a.io/"));
+                Log.i(TAG, "parse query client created");
+            } catch (URISyntaxException e) {
+                Log.i(TAG, "Exception from initializing parse Live Query Client.");
+                e.printStackTrace();
+            }
+        }
+        querySales();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "in on destroy");
+        if (parseLiveQueryClient != null && subscribedQuery != null) {
+            Log.i(TAG, "Unsubscribing live query");
+            parseLiveQueryClient.unsubscribe(subscribedQuery);
+            subscribedQuery = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSalesBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "in on view created");
+        super.onViewCreated(view, savedInstanceState);
+        adapter = new SalesAdapter(getActivity(), items);
+        binding.rvItemsSales.setAdapter(adapter);
+        binding.rvItemsSales.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    public void querySales() {
+        ParseQuery<Item> query = ParseQuery.getQuery(Item.class);
+        query.include("transaction.buyer");
+        query.include(Item.KEY_SELLER);
+        query.whereEqualTo("seller", ParseUser.getCurrentUser());
+        subscribeToLiveQuery(query);
+        query.orderByAscending("status");
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Item>() {
+            @Override
+            public void done(List<Item> itemsFound, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting sales", e);
+                    return;
+                }
+                items.clear();
+                items.addAll(itemsFound);
+                adapter.notifyDataSetChanged();
+                Log.i(TAG, "finished querying sales");
+            }
+        });
+    }
+
+    private void subscribeToLiveQuery(ParseQuery<Item> parseQuery) {
+        if (parseLiveQueryClient != null) {
+            if (subscribedQuery != null) {
+                Log.i(TAG, "Unsubscribing live query");
+                parseLiveQueryClient.unsubscribe(subscribedQuery);
+                subscribedQuery = null;
+            }
+            subscriptionHandling = parseLiveQueryClient.subscribe(parseQuery);
+            Log.i(TAG, "Subscription of live query done.");
+
+            subscriptionHandling.handleEvent(SubscriptionHandling.Event.UPDATE, new SubscriptionHandling.HandleEventCallback<Item>() {
+                @Override
+                public void onEvent(ParseQuery<Item> query, Item updatedItem) {
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Log.i(TAG, "in main looper, UPDATE event");
+                            adapter.handleItemUpdate(updatedItem);
+                        }
+                    });
+                }
+            });
+
+            subscriptionHandling.handleEvent(SubscriptionHandling.Event.ENTER, new SubscriptionHandling.HandleEventCallback<Item>() {
+                @Override
+                public void onEvent(ParseQuery<Item> query, Item addedItem) {
+                    /* Note that this will only happen if the seller is using two devices
+                    * and adds an item on one while keeping the dashboard open in the other.*/
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Log.i(TAG, "in main looper, ENTER event");
+                            adapter.handleItemAdded(addedItem);
+                        }
+                    });
+                }
+            });
+        }
+        subscribedQuery = parseQuery;
+    }
+}
